33fbd010527dcaa92bf4669ca97b48004610369b
Unknown <federico@nearit.com>
2017-09-05 13:45:28 +0200

    fix: new getInstance method call on NearItManager

--- uibindings/src/main/java/com/nearit/ui_bindings/NearITUIBindings.java
+++ uibindings/src/main/java/com/nearit/ui_bindings/NearITUIBindings.java
@@ -1,13 +1,11 @@
 package com.nearit.ui_bindings;
 
-import android.content.Context;
-
 import it.near.sdk.NearItManager;
 
 public class NearITUIBindings {
 
-    public static void init(Context context) {
-        NearItManager.getInstance(context);
+    public static void init() {
+        NearItManager.getInstance();
     }
 
 }
