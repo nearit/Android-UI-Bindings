3d126836b9d9df04eb65e59dbdcced8b246596fb
Federico Boschini <federico@nearit.com>
2017-09-13 17:39:05 +0200

    add a builder for the fragment version

---
+++ nearit-ui/src/main/java/com/nearit/ui_bindings/coupon/CouponDetailFragmentBuilder.java
@@ -0,0 +1,46 @@
+package com.nearit.ui_bindings.coupon;
+
+import android.content.Context;
+
+import it.near.sdk.reactions.couponplugin.model.Coupon;
+
+/**
+ * Created by Federico Boschini on 06/09/17.
+ */
+
+public class CouponDetailFragmentBuilder {
+    private Context mContext;
+    private int mIconDrawable;
+    private int mSeparatorDrawable;
+
+    public CouponDetailFragmentBuilder(Context context) {
+        mContext = context;
+    }
+
+    /**
+     * Sets a custom icon as a placeholder for coupons without icon
+     */
+    public CouponDetailFragmentBuilder setIconPlaceholderResourceId(int icon) {
+        mIconDrawable = icon;
+        return this;
+    }
+
+    /**
+     * Sets a custom separator
+     */
+    public CouponDetailFragmentBuilder setSeparatorResourceId(int separator) {
+        mSeparatorDrawable = separator;
+        return this;
+    }
+
+    public NearItCouponDetailFragment build(Coupon coupon) {
+        return NearItCouponDetailFragment.newInstance(coupon, getParams());
+    }
+
+    private CouponDetailExtraParams getParams() {
+        return new CouponDetailExtraParams(
+                mIconDrawable,
+                mSeparatorDrawable);
+    }
+
+}
