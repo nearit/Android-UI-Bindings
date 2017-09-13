05bd4239d547c16ca71bf6bcfe36479dd54da59e
Federico Boschini <federico@nearit.com>
2017-09-13 11:50:42 +0200

    move coupon detail view to a fragment and add it to an activity

--- nearit-ui/src/main/java/com/nearit/ui_bindings/coupon/NearItCouponDetailActivity.java
+++ nearit-ui/src/main/java/com/nearit/ui_bindings/coupon/NearItCouponDetailActivity.java
@@ -4,16 +4,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.ProgressBar;
-import android.widget.TextView;
 
 import com.nearit.ui_bindings.ExtraConstants;
 import com.nearit.ui_bindings.R;
-import com.nearit.ui_bindings.coupon.views.CouponDetailTopSection;
 
 import it.near.sdk.reactions.couponplugin.model.Coupon;
 
@@ -25,17 +19,7 @@ public class NearItCouponDetailActivity extends AppCompatActivity {
 
     private final static String TAG = "NearItCouponDetailActivity";
 
-    private int iconDrawable = 0;
-    private int separatorDrawable = 0;
-
-    @Nullable
-    private ImageView couponIcon;
-    @Nullable
-    private View separator;
-    @Nullable
-    private ProgressBar iconProgressBar;
-    @Nullable
-    private TextView couponName, couponValue, couponDescription;
+    private CouponDetailIntentExtras extras;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -45,43 +29,14 @@ public class NearItCouponDetailActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         if (intent.hasExtra(ExtraConstants.EXTRA_FLOW_PARAMS)) {
-            CouponDetailIntentExtras params = CouponDetailIntentExtras.fromIntent(intent);
-            iconDrawable = params.getIconDrawable();
-            separatorDrawable = params.getSeparatorDrawable();
+            extras = CouponDetailIntentExtras.fromIntent(intent);
         }
 
         Coupon coupon = getIntent().getParcelableExtra(ExtraConstants.COUPON_EXTRA);
 
-        CouponDetailTopSection topSection = (CouponDetailTopSection) findViewById(R.id.coupon_detail_top_section);
+        NearItCouponDetailFragment fragment = NearItCouponDetailFragment.newInstance(coupon, extras);
 
-        topSection.setCouponView(coupon);
-
-        iconProgressBar = (ProgressBar) findViewById(R.id.coupon_icon_progress_bar);
-        couponIcon = (ImageView) findViewById(R.id.coupon_icon);
-        separator = findViewById(R.id.coupon_detail_separator);
-
-        if (separatorDrawable != 0 && separator != null) {
-            separator.setBackgroundResource(separatorDrawable);
-        }
-
-        couponName = (TextView) findViewById(R.id.coupon_name);
-        couponName.setText(coupon.name);
-        couponValue = (TextView) findViewById(R.id.coupon_value);
-        couponValue.setText(coupon.value);
-        couponDescription = (TextView) findViewById(R.id.coupon_description);
-        couponDescription.setText(coupon.description);
-
-        if ((coupon.getRedeemableFromDate() != null) && (coupon.getRedeemableFromDate().getTime() > System.currentTimeMillis()) || coupon.getRedeemedAtDate() != null) {
-            couponName.setTextColor(ContextCompat.getColor(this, R.color.nearit_ui_coupon_detail_disabled_text_color));
-            couponValue.setTextColor(ContextCompat.getColor(this, R.color.nearit_ui_coupon_detail_disabled_text_color));
-            couponDescription.setTextColor(ContextCompat.getColor(this, R.color.nearit_ui_coupon_detail_disabled_text_color));
-        }
-
-        if (iconDrawable != 0 && couponIcon != null) {
-            couponIcon.setBackgroundResource(iconDrawable);
-        }
-
-//        new LoadImageFromURL(couponIcon, iconProgressBar).execute(coupon.getIconSet().getFullSize());
+        getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, fragment).commit();
 
     }
 
