30a4ce713c66539be8d16a24ed0c18bf828cde2d
Federico Boschini <federico@nearit.com>
2017-09-15 18:19:44 +0200

    modify feedback activity

--- nearit-ui/src/main/java/com/nearit/ui_bindings/feedback/NearItFeedbackActivity.java
+++ nearit-ui/src/main/java/com/nearit/ui_bindings/feedback/NearItFeedbackActivity.java
@@ -4,20 +4,36 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Rect;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
+import android.widget.RatingBar;
+import android.widget.Toast;
 
 import com.nearit.ui_bindings.ExtraConstants;
 import com.nearit.ui_bindings.R;
-import com.nearit.ui_bindings.permissions.NearItPermissionsActivity;
-import com.nearit.ui_bindings.permissions.PermissionsRequestIntentExtras;
 
 public class NearItFeedbackActivity extends AppCompatActivity {
+
+    @Nullable
+    RatingBar ratingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_near_it_feedback);
+        setContentView(R.layout.nearit_ui_activity_feedback);
+
+        ratingBar = (RatingBar) findViewById(R.id.feedback_rating);
+
+        if (ratingBar != null) {
+            ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+                @Override
+                public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                    Toast.makeText(NearItFeedbackActivity.this, Float.toString(rating), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
     }
 
     public static Intent createIntent(Context context, FeedbackRequestIntentExtras params) {
