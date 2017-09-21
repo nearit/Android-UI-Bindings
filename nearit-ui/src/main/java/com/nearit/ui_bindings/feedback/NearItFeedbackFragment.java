a47c784d4e2037974b8c1445db072d9a8745fe2c
Federico Boschini <federico@nearit.com>
2017-09-21 11:35:00 +0200

    add positive result view for feedback response

--- nearit-ui/src/main/java/com/nearit/ui_bindings/feedback/NearItFeedbackFragment.java
+++ nearit-ui/src/main/java/com/nearit/ui_bindings/feedback/NearItFeedbackFragment.java
@@ -1,6 +1,7 @@
 package com.nearit.ui_bindings.feedback;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.Parcelable;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -8,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RatingBar;
 import android.widget.TextView;
@@ -25,6 +27,10 @@ public class NearItFeedbackFragment extends Fragment {
     private static final String ARG_EXTRAS = "extras";
     private static final String SAVED_RATING = "rating";
     private static final String SAVED_IS_BUTTON_CHECKED = "button_state";
+    private static final String SAVED_IS_ALERT_VISIBLE = "alert_state";
+
+
+    private static int NEAR_AUTOCLOSE_DELAY = 2000;
 
     private Feedback feedback;
     private boolean hideTextResponse = false;
@@ -35,13 +41,17 @@ public class NearItFeedbackFragment extends Fragment {
     @Nullable
     RatingBar ratingBar;
     @Nullable
-    LinearLayout commentSection;
+    LinearLayout commentSection, ratingBarContainer;
     @Nullable
     NearItUIFeedbackButton sendButton;
     @Nullable
     TextView closeButton, errorText, feedbackQuestionTextView;
     @Nullable
     EditText commentBox;
+    @Nullable
+    ImageView positiveResultIcon;
+    @Nullable
+    TextView positiveResultMessage;
 
     public NearItFeedbackFragment() {
     }
@@ -77,6 +87,10 @@ public class NearItFeedbackFragment extends Fragment {
         if (sendButton != null) {
             outState.putBoolean(SAVED_IS_BUTTON_CHECKED, sendButton.isChecked());
         }
+        if (errorText != null) {
+            outState.putBoolean(SAVED_IS_ALERT_VISIBLE, errorText.isShown());
+        }
+
     }
 
     @Override
@@ -88,10 +102,17 @@ public class NearItFeedbackFragment extends Fragment {
                 ratingBar.setRating(savedInstanceState.getFloat(SAVED_RATING));
             }
             if (sendButton != null) {
-                if(savedInstanceState.getBoolean(SAVED_IS_BUTTON_CHECKED)) {
+                if (savedInstanceState.getBoolean(SAVED_IS_BUTTON_CHECKED)) {
                     sendButton.setChecked();
                 } else {
                     sendButton.setUnchecked();
+                }
+            }
+            if (errorText != null) {
+                if (savedInstanceState.getBoolean(SAVED_IS_ALERT_VISIBLE)) {
+                    errorText.setVisibility(View.VISIBLE);
+                } else {
+                    errorText.setVisibility(View.GONE);
                 }
             }
         }
@@ -103,13 +124,17 @@ public class NearItFeedbackFragment extends Fragment {
 
         feedbackQuestion = feedback.question;
 
+        ratingBarContainer = (LinearLayout) rootView.findViewById(R.id.feedback_rating_bar_container);
         ratingBar = (RatingBar) rootView.findViewById(R.id.feedback_rating);
         commentSection = (LinearLayout) rootView.findViewById(R.id.feedback_comment_section);
+        errorText = (TextView) rootView.findViewById(R.id.feedback_error_alert);
         sendButton = (NearItUIFeedbackButton) rootView.findViewById(R.id.feedback_send_comment_button);
         closeButton = (TextView) rootView.findViewById(R.id.feedback_dismiss_text);
         commentBox = (EditText) rootView.findViewById(R.id.feedback_comment_box);
-        errorText = (TextView) rootView.findViewById(R.id.feedback_error_alert);
         feedbackQuestionTextView = (TextView) rootView.findViewById(R.id.feedback_question);
+
+        positiveResultMessage = (TextView) rootView.findViewById(R.id.feedback_success_message);
+        positiveResultIcon = (ImageView) rootView.findViewById(R.id.feedback_success_icon);
 
         if (feedbackQuestionTextView != null) {
             feedbackQuestionTextView.setText(feedbackQuestion);
@@ -166,7 +191,40 @@ public class NearItFeedbackFragment extends Fragment {
                     NearItManager.getInstance().sendEvent(new FeedbackEvent(feedback, (int) userRating, userComment), new NearITEventHandler() {
                         @Override
                         public void onSuccess() {
-                            getActivity().finish();
+                            if (sendButton != null) {
+                                sendButton.setVisibility(View.GONE);
+                            }
+                            if (ratingBarContainer != null) {
+                                ratingBarContainer.setVisibility(View.GONE);
+                            }
+                            if (feedbackQuestionTextView != null) {
+                                feedbackQuestionTextView.setVisibility(View.GONE);
+                            }
+                            if (commentSection != null) {
+                                commentSection.setVisibility(View.GONE);
+                            }
+                            if (errorText != null) {
+                                errorText.setVisibility(View.GONE);
+                            }
+                            if (closeButton != null) {
+                                closeButton.setVisibility(View.GONE);
+                            }
+                            if (positiveResultMessage != null) {
+                                positiveResultMessage.setVisibility(View.VISIBLE);
+                            }
+                            if (positiveResultIcon != null) {
+                                positiveResultIcon.setVisibility(View.VISIBLE);
+                            }
+
+                            final Handler handler = new Handler();
+                            handler.postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    if (getActivity() != null) {
+                                        getActivity().finish();
+                                    }
+                                }
+                            }, NEAR_AUTOCLOSE_DELAY);
                         }
 
                         @Override
