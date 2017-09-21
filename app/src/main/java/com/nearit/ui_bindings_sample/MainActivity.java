267e20f2709cad29644e52113ad985d92c441550
Federico Boschini <federico@nearit.com>
2017-09-21 11:34:11 +0200

    (sample) add receiver for real NearIT contents

--- app/src/main/java/com/nearit/ui_bindings_sample/MainActivity.java
+++ app/src/main/java/com/nearit/ui_bindings_sample/MainActivity.java
@@ -2,7 +2,9 @@ package com.nearit.ui_bindings_sample;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -15,8 +17,11 @@ import it.near.sdk.reactions.feedbackplugin.model.Feedback;
 import it.near.sdk.reactions.simplenotificationplugin.model.SimpleNotification;
 import it.near.sdk.trackings.TrackingInfo;
 import it.near.sdk.utils.CoreContentsListener;
+import it.near.sdk.utils.NearUtils;
 
 public class MainActivity extends AppCompatActivity implements CoreContentsListener{
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,30 +54,53 @@ public class MainActivity extends AppCompatActivity implements CoreContentsListe
 
     }
 
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        onNewIntent(getIntent());
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        if (intent != null &&
+                intent.getExtras() != null &&
+                NearUtils.carriesNearItContent(intent)) {
+
+            NearUtils.parseCoreContents(intent, this);
+        }
+    }
+
     //  The following methods will manage real NearIT notification
 
     @Override
     public void gotContentNotification(Content content, TrackingInfo trackingInfo) {
         //  Not yet implemented
+        Log.d(TAG, "content notification received");
     }
 
     @Override
     public void gotCouponNotification(Coupon coupon, TrackingInfo trackingInfo) {
+        Log.d(TAG, "coupon notification received");
         startActivity(NearITUIBindings.getInstance(this).createCouponDetailIntentBuilder(coupon).build());
     }
 
     @Override
     public void gotCustomJSONNotification(CustomJSON customJSON, TrackingInfo trackingInfo) {
         //  Not yet implemented
+        Log.d(TAG, "customJson notification received");
     }
 
     @Override
     public void gotSimpleNotification(SimpleNotification simpleNotification, TrackingInfo trackingInfo) {
         //  Not yet implemented
+        Log.d(TAG, "simple notification received");
     }
 
     @Override
     public void gotFeedbackNotification(Feedback feedback, TrackingInfo trackingInfo) {
+        Log.d(TAG, "feedback notification received");
         startActivity(NearITUIBindings.getInstance(this).createFeedbackIntentBuilder(feedback).build());
     }
 }
