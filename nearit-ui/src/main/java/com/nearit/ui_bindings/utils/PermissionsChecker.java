dc1b386e412d5504a431d7278141e355596f2cd2
Federico Boschini <federico@nearit.com>
2017-09-28 12:47:20 +0200

    add permissionchecker

---
+++ nearit-ui/src/main/java/com/nearit/ui_bindings/utils/PermissionsChecker.java
@@ -0,0 +1,54 @@
+package com.nearit.ui_bindings.utils;
+
+import android.bluetooth.BluetoothAdapter;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.support.v4.content.ContextCompat;
+
+import it.near.sdk.logging.NearLog;
+
+/**
+ * Created by Federico Boschini on 28/09/17.
+ */
+
+public class PermissionsChecker {
+
+    private final static String TAG = "PermissionsChecker";
+    private Context context;
+
+    public PermissionsChecker(Context context) {
+        this.context = context;
+    }
+
+    public boolean checkLocationPermission() {
+        return ContextCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    public boolean checkLocation() {
+        LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) | locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+    }
+
+    public boolean checkBluetooth() {
+        return BluetoothAdapter.getDefaultAdapter() != null && BluetoothAdapter.getDefaultAdapter().isEnabled();
+    }
+
+
+    /**
+     * Checks for BLE availability
+     */
+    private boolean isBleAvailable() {
+        boolean available = false;
+        if (android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.JELLY_BEAN_MR2) {
+            NearLog.d(TAG, "BLE not supported prior to API 18");
+        } else if (!context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            NearLog.d(TAG, "BLE not available on this device");
+        } else {
+            available = true;
+        }
+        return available;
+    }
+
+
+}
