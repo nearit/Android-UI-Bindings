c7540daf0f19bac4157349b21ca08b34f209020e
Federico Boschini <federico@nearit.com>
2017-10-04 19:02:39 +0200

    add content notification layout

--- nearit-ui/src/main/java/com/nearit/ui_bindings/content/NearItContentDetailFragment.java
+++ nearit-ui/src/main/java/com/nearit/ui_bindings/content/NearItContentDetailFragment.java
@@ -1,21 +1,35 @@
 package com.nearit.ui_bindings.content;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.nearit.ui_bindings.R;
+import com.nearit.ui_bindings.content.views.ContentCTAButton;
+import com.nearit.ui_bindings.utils.LoadImageFromURL;
 
 import it.near.sdk.reactions.contentplugin.model.Content;
+
 
 public class NearItContentDetailFragment extends Fragment {
     private static final String ARG_CONTENT = "content";
     private static final String ARG_EXTRAS = "extras";
 
     private Content content;
+    private TextView titleTextView, contentTextView;
+    private ContentCTAButton ctaButton;
+    private ImageView contentImageView;
+    private ProgressBar contentImageSpinner;
+    private LinearLayout contentImageContainer;
 
     public NearItContentDetailFragment() {
     }
@@ -44,6 +58,39 @@ public class NearItContentDetailFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.nearit_ui_fragment_content_detail, container, false);
+
+        titleTextView = (TextView) rootView.findViewById(R.id.content_title);
+        contentTextView = (TextView) rootView.findViewById(R.id.content_text);
+        ctaButton = (ContentCTAButton) rootView.findViewById(R.id.cta_button);
+        contentImageView = (ImageView) rootView.findViewById(R.id.content_image);
+        contentImageSpinner = (ProgressBar) rootView.findViewById(R.id.content_image_progress_bar);
+        contentImageContainer = (LinearLayout) rootView.findViewById(R.id.content_image_container);
+
+        if (content.title != null) {
+            titleTextView.setVisibility(View.VISIBLE);
+            titleTextView.setText(content.title);
+        }
+
+        if (content.contentString != null) {
+            contentTextView.setVisibility(View.VISIBLE);
+            contentTextView.setText(content.contentString);
+        }
+
+        if (content.getImageLink() != null) {
+            contentImageContainer.setVisibility(View.VISIBLE);
+            new LoadImageFromURL(contentImageView, contentImageSpinner).execute(content.getImageLink().getFullSize());
+        }
+
+        if (content.getCta() != null) {
+            ctaButton.setVisibility(View.VISIBLE);
+            ctaButton.setText(content.getCta().label);
+            ctaButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(content.getCta().url)));
+                }
+            });
+        }
 
         return rootView;
     }
