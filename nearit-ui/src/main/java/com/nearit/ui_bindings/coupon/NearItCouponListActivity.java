23df63a6110d7d7b957fbea254a14a5cd62c5cba
Cattaneo Stefano <stefano@nearit.com>
2018-02-16 13:13:12 +0100

    coupon list params constructors, default and annotations

--- nearit-ui/src/main/java/com/nearit/ui_bindings/coupon/NearItCouponListActivity.java
+++ nearit-ui/src/main/java/com/nearit/ui_bindings/coupon/NearItCouponListActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Rect;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
@@ -18,8 +19,6 @@ import com.nearit.ui_bindings.R;
 public class NearItCouponListActivity extends AppCompatActivity {
 
     private final static String TAG = "NearItCouponListActiv";
-
-    private CouponListExtraParams extras;
     private boolean isEnableTapToClose;
 
     @Override
@@ -28,8 +27,10 @@ public class NearItCouponListActivity extends AppCompatActivity {
 
         setContentView(R.layout.nearit_ui_activity_coupon_list);
 
+        CouponListExtraParams extras = null;
         Intent intent = getIntent();
-        if (intent.hasExtra(ExtraConstants.EXTRA_FLOW_PARAMS)) {
+        if (intent != null &&
+                intent.hasExtra(ExtraConstants.EXTRA_FLOW_PARAMS)) {
             extras = CouponListExtraParams.fromIntent(intent);
             isEnableTapToClose = extras.isEnableTapOutsideToClose();
         }
@@ -41,6 +42,7 @@ public class NearItCouponListActivity extends AppCompatActivity {
 
     }
 
+    @NonNull
     public static Intent createIntent(Context context, CouponListExtraParams params) {
         return new Intent(context, NearItCouponListActivity.class)
                 .putExtra(ExtraConstants.EXTRA_FLOW_PARAMS, params);
