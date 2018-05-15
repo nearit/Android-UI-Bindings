3ea6cc61d76b02339506943a0347d0d10826697d
Federico Boschini <federico@nearit.com>
2018-05-15 13:11:20 +0200

    feat(SAMPLE): use new filters

--- sample/src/main/java/com/your_company/ui_bindings_sample/CouponsActivity.java
+++ sample/src/main/java/com/your_company/ui_bindings_sample/CouponsActivity.java
@@ -4,9 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 
 import com.nearit.ui_bindings.NearITUIBindings;
+import com.nearit.ui_bindings.coupon.CouponDetailIntentBuilder;
 import com.your_company.ui_bindings_sample.factories.CouponFactory;
 
 import it.near.sdk.reactions.couponplugin.model.Coupon;
@@ -17,83 +17,18 @@ import it.near.sdk.reactions.couponplugin.model.Coupon;
 
 public class CouponsActivity extends AppCompatActivity {
 
+    CouponFactory couponFactory;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coupons);
 
-        final CouponFactory couponFactory = new CouponFactory();
+        couponFactory = new CouponFactory();
 
-        Button valid = findViewById(R.id.valid_coupon);
-        valid.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //  In a real scenario the coupon is provided by the NearIT SDK
-                Coupon validCoupon = couponFactory.getValidCoupon();
-                startActivity(
-                        //  Basic example with a valid coupon
-                        NearITUIBindings.getInstance(CouponsActivity.this)
-                                .createCouponDetailIntentBuilder(validCoupon)
-                                .build());
-            }
-        });
+        CouponDetailIntentBuilder builder = new CouponDetailIntentBuilder(this, null, true);
 
-        Button inactive = findViewById(R.id.inactive_coupon);
-        inactive.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //  In a real scenario the coupon is provided by the NearIT SDK
-                Coupon inactiveCoupon = couponFactory.getInactiveCoupon();
-                startActivity(
-                        //  Basic example with a coupon not yet valid
-                        NearITUIBindings.getInstance(CouponsActivity.this)
-                                .createCouponDetailIntentBuilder(inactiveCoupon)
-                                .build());
-            }
-        });
-
-        Button expired = findViewById(R.id.expired_coupon);
-        expired.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //  In a real scenario the coupon is provided by the NearIT SDK
-                Coupon expiredCoupon = couponFactory.getExpiredCoupon();
-                startActivity(
-                        //  Basic example with an expired coupon
-                        //  + enable tap outside the dialog to close it
-                        NearITUIBindings.getInstance(CouponsActivity.this)
-                                .createCouponDetailIntentBuilder(expiredCoupon)
-                                .enableTapOutsideToClose()
-                                .build());
-            }
-        });
-
-        Button customIcon = findViewById(R.id.custom_icon_coupon);
-        customIcon.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //  In a real scenario the coupon is provided by the NearIT SDK
-                Coupon validCoupon = couponFactory.getValidCoupon();
-                startActivity(
-                        //  Change the default icon placeholder
-                        NearITUIBindings.getInstance(CouponsActivity.this)
-                                .createCouponDetailIntentBuilder(validCoupon)
-                                .setIconPlaceholderResourceId(R.drawable.my_custom_placeholder)
-                                .build());
-            }
-        });
-
-        Button plainActivity = findViewById(R.id.plain_coupon_activity);
-        plainActivity.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //  start a plain activity that shows the fragment fullscreen
-                //  please check it
-                startActivity(new Intent(CouponsActivity.this, CouponPlainActivity.class));
-            }
-        });
-
-
+        /*
 
         Button couponList = findViewById(R.id.coupon_list);
         couponList.setOnClickListener(new View.OnClickListener() {
@@ -106,82 +41,140 @@ public class CouponsActivity extends AppCompatActivity {
                         .build()
                 );
             }
-        });
+        }); */
 
-        Button validCouponList = findViewById(R.id.valid_coupon_list);
-        validCouponList.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //  start an activity that shows a list of REAL valid coupons ONLY
-                startActivity(NearITUIBindings.getInstance(CouponsActivity.this)
-                        .createCouponListIntentBuilder()
-                        .onlyValidCoupons()
-                        .build()
-                );
-            }
-        });
+    }
 
-        Button expiredCouponList = findViewById(R.id.expired_coupon_list);
-        expiredCouponList.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //  start an activity that shows a list of REAL expired coupons ONLY
-                startActivity(NearITUIBindings.getInstance(CouponsActivity.this)
-                        .createCouponListIntentBuilder()
-                        .onlyExpiredCoupons()
-                        .build()
-                );
-            }
-        });
 
-        Button inactiveCouponList = findViewById(R.id.inactive_coupon_list);
-        inactiveCouponList.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //  start an activity that shows a list of REAL inactive coupons ONLY
-                startActivity(NearITUIBindings.getInstance(CouponsActivity.this)
-                        .createCouponListIntentBuilder()
-                        .onlyInactiveCoupons()
-                        .build()
-                );
-            }
-        });
 
-        Button redeemedCouponList = findViewById(R.id.redeemed_coupon_list);
-        redeemedCouponList.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //  start an activity that shows a list of REAL already redeemed coupons ONLY
-                startActivity(NearITUIBindings.getInstance(CouponsActivity.this)
-                        .createCouponListIntentBuilder()
-                        .onlyRedeemedCoupons()
-                        .build()
-                );
-            }
-        });
+    public void onDefaultClicked(View view) {
+        startActivity(NearITUIBindings.getInstance(CouponsActivity.this)
+                .createCouponListIntentBuilder()
+                .jaggedBorders()
+                .build());
+    }
 
-        Button completeList = findViewById(R.id.include_redeemed_coupon_list);
-        completeList.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //  start an activity that shows the complete list of REAL coupons (redeemed included)
-                startActivity(NearITUIBindings.getInstance(CouponsActivity.this)
-                        .createCouponListIntentBuilder()
-                        .includeRedeemed()
-                        .build()
-                );
-            }
-        });
+    public void onValidClicked(View view) {
+        startActivity(NearITUIBindings.getInstance(CouponsActivity.this)
+                .createCouponListIntentBuilder()
+                .validCoupons()
+                .build());
+    }
 
-        Button listInActivity = findViewById(R.id.coupon_list_in_activity);
-        listInActivity.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //  start a plain activity that shows the fragment fullscreen
-                //  please check it
-                startActivity(new Intent(CouponsActivity.this, CouponListPlainActivity.class));
-            }
-        });
+    public void onInactiveClicked(View view) {
+        startActivity(NearITUIBindings.getInstance(CouponsActivity.this)
+                .createCouponListIntentBuilder()
+                .inactiveCoupons()
+                .build());
+    }
 
+    public void onExpiredClicked(View view) {
+        startActivity(NearITUIBindings.getInstance(CouponsActivity.this)
+                .createCouponListIntentBuilder()
+                .expiredCoupons()
+                .build());
+    }
+
+    public void onRedeemedClicked(View view) {
+        startActivity(NearITUIBindings.getInstance(CouponsActivity.this)
+                .createCouponListIntentBuilder()
+                .redeemedCoupons()
+                .build());
+    }
+
+    public void onValidAndInactiveClicked(View view) {
+        startActivity(NearITUIBindings.getInstance(CouponsActivity.this)
+                .createCouponListIntentBuilder()
+                .validCoupons()
+                .inactiveCoupons()
+                .build());
+    }
+
+    public void onValidAndExpiredClicked(View view) {
+        startActivity(NearITUIBindings.getInstance(CouponsActivity.this)
+                .createCouponListIntentBuilder()
+                .validCoupons()
+                .expiredCoupons()
+                .build());
+    }
+
+    public void onValidAndRedeemedClicked(View view) {
+        startActivity(NearITUIBindings.getInstance(CouponsActivity.this)
+                .createCouponListIntentBuilder()
+                .validCoupons()
+                .redeemedCoupons()
+                .build());
+    }
+
+    public void onInactiveAndExpiredClicked(View view) {
+        startActivity(NearITUIBindings.getInstance(CouponsActivity.this)
+                .createCouponListIntentBuilder()
+                .inactiveCoupons()
+                .expiredCoupons()
+                .build());
+    }
+
+    public void onInactiveAndRedeemedClicked(View view) {
+        startActivity(NearITUIBindings.getInstance(CouponsActivity.this)
+                .createCouponListIntentBuilder()
+                .inactiveCoupons()
+                .redeemedCoupons()
+                .build());
+    }
+
+    public void onExpiredAndRedeemedClicked(View view) {
+        startActivity(NearITUIBindings.getInstance(CouponsActivity.this)
+                .createCouponListIntentBuilder()
+                .expiredCoupons()
+                .redeemedCoupons()
+                .build());
+    }
+
+    public void onValidDetailClicked(View view) {
+        //  In a real scenario the coupon is provided by the NearIT SDK
+        Coupon validCoupon = couponFactory.getValidCoupon();
+        startActivity(
+                //  Basic example with a valid coupon
+                NearITUIBindings.getInstance(CouponsActivity.this)
+                        .createCouponDetailIntentBuilder(validCoupon)
+                        .build());
+    }
+
+    public void onInactiveDetailClicked(View view) {
+        //  In a real scenario the coupon is provided by the NearIT SDK
+        Coupon validCoupon = couponFactory.getInactiveCoupon();
+        startActivity(
+                //  Basic example with a not yet valid coupon
+                NearITUIBindings.getInstance(CouponsActivity.this)
+                        .createCouponDetailIntentBuilder(validCoupon)
+                        .build());
+    }
+
+    public void onExpiredDetailClicked(View view) {
+        Coupon expiredCoupon = couponFactory.getExpiredCoupon();
+        startActivity(
+                //  Basic example with an expired coupon
+                //  + enable tap outside the dialog to close it
+                NearITUIBindings.getInstance(CouponsActivity.this)
+                        .createCouponDetailIntentBuilder(expiredCoupon)
+                        .enableTapOutsideToClose()
+                        .build());
+    }
+
+    public void onCustomIconDetailClicked(View view) {
+        //  In a real scenario the coupon is provided by the NearIT SDK
+        Coupon validCoupon = couponFactory.getValidCoupon();
+        startActivity(
+                //  Change the default icon placeholder
+                NearITUIBindings.getInstance(CouponsActivity.this)
+                        .createCouponDetailIntentBuilder(validCoupon)
+                        .setIconPlaceholderResourceId(R.drawable.my_custom_placeholder)
+                        .build());
+    }
+
+    public void onPlainCouponClicked(View view) {
+        //  start a plain activity that shows the fragment fullscreen
+        //  please check it
+        startActivity(new Intent(CouponsActivity.this, CouponPlainActivity.class));
     }
 }
