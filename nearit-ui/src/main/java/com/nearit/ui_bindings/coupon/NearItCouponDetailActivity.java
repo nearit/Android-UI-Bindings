f024a2d85aa5d4fa3c4d8001b15c29a09c25f7dd
Federico Boschini <federico@nearit.com>
2018-07-09 16:31:23 +0200

    refactor: optimize imports

--- nearit-ui/src/main/java/com/nearit/ui_bindings/coupon/NearItCouponDetailActivity.java
+++ nearit-ui/src/main/java/com/nearit/ui_bindings/coupon/NearItCouponDetailActivity.java
@@ -2,16 +2,6 @@ package com.nearit.ui_bindings.coupon;
 
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Rect;
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
-import android.view.MotionEvent;
-import android.view.View;
-import android.widget.LinearLayout;
-
-import com.nearit.ui_bindings.ExtraConstants;
-import com.nearit.ui_bindings.R;
 
 import it.near.sdk.reactions.couponplugin.model.Coupon;
 
