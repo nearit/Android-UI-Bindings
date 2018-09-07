ed1b9960f27ae56c2cd66485424ac2e1d7dae96b
Federico Boschini <federico@nearit.com>
2018-09-07 11:38:57 +0200

    feat(customTabs): define webViewActivity

--- nearit-ui/src/main/java/com/nearit/customtabs/WebViewActivity.java
+++ nearit-ui/src/main/java/com/nearit/customtabs/WebViewActivity.java
@@ -1,85 +1,88 @@
-/*
- * Copyright 2017 Sascha Peilicke
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2015 Google Inc. All Rights Reserved.
+//
+// EDIT by Federico Boschini <federico@nearit.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package com.nearit.customtabs;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 import com.nearit.ui_bindings.R;
 
-public final class WebViewActivity extends AppCompatActivity {
-    /**
-     * Optional title resource for the actionbar / toolbar.
-     */
-    public static final String EXTRA_TITLE = WebViewActivity.class.getName() + ".EXTRA_TITLE";
+/**
+ * This Activity is used as a fallback when there is no browser installed that supports
+ * Chrome Custom Tabs
+ */
+public class WebViewActivity extends AppCompatActivity {
 
-    /**
-     * Mandatory file to load and display.
-     */
-    public static final String EXTRA_URL = WebViewActivity.class.getName() + ".EXTRA_URL";
+    public static final String EXTRA_URL = "extra.url";
+    public static final String EXTRA_HOME_AS_UP_INDICATOR = "extra.home.as.up";
+
+    private WebView webView;
 
     @SuppressLint("SetJavaScriptEnabled")
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.nearit_ui_activity_webview);
 
-        String title = getIntent().getStringExtra(EXTRA_TITLE);
-        String url = getIntent().getStringExtra(EXTRA_URL);
+        webView = findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        setTitle("culo");
 
-        final ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true);
-            if (title != null) {
-                actionBar.setTitle(title);
-                actionBar.setSubtitle(url);
-            } else {
-                actionBar.setTitle(url);
-            }
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
+    }
 
-        WebView webView = findViewById(R.id.web_view);
-        webView.loadUrl(url);
-        webView.getSettings().setJavaScriptEnabled(true);
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        if (getIntent() != null) {
 
-        // No title provided. Use the website's once it's loaded...
-        if (title == null) {
-            webView.setWebViewClient(new WebViewClient() {
-                @Override
-                public void onPageFinished(WebView view, String url) {
-                    super.onPageFinished(view, url);
-                    if (actionBar != null) {
-                        actionBar.setTitle(view.getTitle());
-                        actionBar.setSubtitle(url);
+            if (getIntent().hasExtra(EXTRA_URL)) {
+                String url = getIntent().getStringExtra(EXTRA_URL);
+                setTitle(url);
+                webView.loadUrl(url);
+            }
+            if (getIntent().hasExtra(EXTRA_HOME_AS_UP_INDICATOR)) {
+                int homeAsUpIndicatorResId = getIntent().getIntExtra(EXTRA_HOME_AS_UP_INDICATOR, 0);
+                if (getSupportActionBar() != null) {
+                    if (homeAsUpIndicatorResId != 0) {
+                        getSupportActionBar().setHomeAsUpIndicator(homeAsUpIndicatorResId);
                     }
                 }
-            });
+            }
+
+
         }
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
             case android.R.id.home:
                 finish();
                 return true;
