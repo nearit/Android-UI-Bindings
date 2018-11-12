2e99b9b56593fd086a3fc609edfdb0da06d24b2e
Federico Boschini <federico@nearit.com>
2018-11-12 17:48:48 +0100

    refactor(imageDownloader): simplify async task methods

--- nearit-ui/src/main/java/com/nearit/ui_bindings/utils/LoadImageFromURL.java
+++ nearit-ui/src/main/java/com/nearit/ui_bindings/utils/LoadImageFromURL.java
@@ -3,10 +3,9 @@ package com.nearit.ui_bindings.utils;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
-import android.support.annotation.Nullable;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.ProgressBar;
+import android.support.annotation.NonNull;
+
+import com.nearit.ui_bindings.ImageDownloadListener;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -17,86 +16,28 @@ import java.net.URL;
  */
 
 public final class LoadImageFromURL extends AsyncTask<String, Void, Bitmap> {
-    @Nullable
-    private final ImageView imageView;
-    @Nullable
-    private final ProgressBar progressBar;
-    private final boolean enableReload;
 
-    private String url;
-    private static final boolean ENABLE_RELOAD_DEFAULT = false;
-    private static final ProgressBar PROGRESS_BAR_DEFAULT = null;
+    private final ImageDownloadListener listener;
 
-    public LoadImageFromURL(@Nullable ImageView imageView) {
-        this(imageView, PROGRESS_BAR_DEFAULT);
-    }
-
-    public LoadImageFromURL(@Nullable ImageView imageView, @Nullable ProgressBar progressBar) {
-        this(imageView, progressBar, ENABLE_RELOAD_DEFAULT);
-    }
-
-    public LoadImageFromURL(@Nullable ImageView imageView, @Nullable ProgressBar progressBar, boolean enableReload) {
-        this.imageView = imageView;
-        this.progressBar = progressBar;
-        this.enableReload = enableReload;
-    }
-
-    @Override
-    protected void onPreExecute() {
-        if (imageView != null) {
-            imageView.setVisibility(View.GONE);
-        }
-        if (progressBar != null) {
-            progressBar.setVisibility(View.VISIBLE);
-        }
+    public LoadImageFromURL(@NonNull ImageDownloadListener listener) {
+        this.listener = listener;
     }
 
     @Override
     protected Bitmap doInBackground(String... urls) {
         String url = urls[0];
-        this.url = url;
         Bitmap icon = null;
         InputStream in;
         try {
             in = new URL(url).openStream();
             icon = BitmapFactory.decodeStream(in);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        } catch (IOException ignored) { }
         return icon;
     }
 
     @Override
     protected void onPostExecute(Bitmap icon) {
-        if (imageView != null) {
-            imageView.setVisibility(View.VISIBLE);
-            if (icon != null) {
-                //  show icon
-                imageView.setScaleType(ImageView.ScaleType.FIT_XY);
-                imageView.setAdjustViewBounds(true);
-                imageView.setMinimumHeight(0);
-                /*if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-                    imageView.setBackground(null);
-                } else {
-                    imageView.setBackgroundDrawable(null);
-                }*/
-                imageView.setImageBitmap(icon);
-            } else {
-                //  show reload
-                if (enableReload) {
-                    imageView.setScaleType(ImageView.ScaleType.CENTER);
-                    imageView.setAdjustViewBounds(false);
-                    imageView.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
-                            new LoadImageFromURL(imageView, progressBar).execute(url);
-                        }
-                    });
-                }
-            }
-        }
-        if (progressBar != null) {
-            progressBar.setVisibility(View.GONE);
-        }
+        if (icon != null) listener.onSuccess(icon);
+        else listener.onError();
     }
 }
