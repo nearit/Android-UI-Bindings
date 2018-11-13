4fb60a1475f355cabd770c1fbba8dc5d9f4fd81e
Federico Boschini <federico@nearit.com>
2018-11-13 16:37:11 +0100

    refactor(imageDownloader): remove method with timeout

--- nearit-ui/src/main/java/com/nearit/ui_bindings/utils/images/NearItImageDownloader.java
+++ nearit-ui/src/main/java/com/nearit/ui_bindings/utils/images/NearItImageDownloader.java
@@ -4,10 +4,6 @@ import android.graphics.Bitmap;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
-
 import it.near.sdk.logging.NearLog;
 
 /**
@@ -64,34 +60,6 @@ public class NearItImageDownloader {
                 });
 
                 task.execute(url);
-                tasksManager.addTask(url, task);
-            }
-        } else listener.onSuccess(cached);
-    }
-
-    public void downloadImage(@NonNull final String url, @NonNull final ImageDownloadListener listener, int timeout) throws InterruptedException, ExecutionException, TimeoutException {
-        Bitmap cached = cacheManager.getBitmapFromMemCache(url);
-        if (cached == null) {
-            if (tasksManager.isAlreadyRunning(url)) {
-                NearLog.d(TAG, String.format("A background task for %s is already running", url));
-                tasksManager.getTask(url).addListener(listener);
-            } else {
-                final LoadImageFromURL task = tasksManager.createNewTask(new ImageDownloadListener() {
-                    @Override
-                    public void onSuccess(@NonNull Bitmap bitmap) {
-                        cacheManager.addBitmapToMemoryCache(url, bitmap);
-                        listener.onSuccess(bitmap);
-                        tasksManager.removeTask(url);
-                    }
-
-                    @Override
-                    public void onError() {
-                        listener.onError();
-                        tasksManager.removeTask(url);
-                    }
-                });
-
-                task.execute(url).get(timeout, TimeUnit.MILLISECONDS);
                 tasksManager.addTask(url, task);
             }
         } else listener.onSuccess(cached);
