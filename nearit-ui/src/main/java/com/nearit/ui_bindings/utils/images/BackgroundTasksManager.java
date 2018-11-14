ccd32e761b9e7ba7de63de1d2dcf7b55d6d632f8
Federico Boschini <federico@nearit.com>
2018-11-14 12:34:22 +0100

    refactor(tasksManager): make everything package-private

--- nearit-ui/src/main/java/com/nearit/ui_bindings/utils/images/BackgroundTasksManager.java
+++ nearit-ui/src/main/java/com/nearit/ui_bindings/utils/images/BackgroundTasksManager.java
@@ -8,27 +8,27 @@ import java.util.Map;
 /**
  * @author Federico Boschini
  */
-public class BackgroundTasksManager {
+class BackgroundTasksManager {
 
     private final Map<String, LoadImageFromURL> backgroundTasks = new HashMap<>();
 
-    public boolean isAlreadyRunning(@NonNull String url) {
+    boolean isAlreadyRunning(@NonNull String url) {
         return backgroundTasks.containsKey(url);
     }
 
-    public LoadImageFromURL getTask(@NonNull String url) {
+    LoadImageFromURL getTask(@NonNull String url) {
         return backgroundTasks.get(url);
     }
 
-    public void removeTask(@NonNull String url) {
+    void removeTask(@NonNull String url) {
         backgroundTasks.remove(url);
     }
 
-    public void addTask(@NonNull String url, @NonNull LoadImageFromURL task) {
+    void addTask(@NonNull String url, @NonNull LoadImageFromURL task) {
         backgroundTasks.put(url, task);
     }
 
-    public LoadImageFromURL createNewTask(@NonNull ImageDownloadListener listener) {
+    LoadImageFromURL createNewTask(@NonNull ImageDownloadListener listener) {
         return new LoadImageFromURL(listener);
     }
 
