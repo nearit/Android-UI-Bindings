ac945f021e13d77dd628f42f3e1e11b422c84142
Federico Boschini <federico@nearit.com>
2018-11-14 15:03:07 +0100

    refactor(imageDownloader): onSuccess return an Image

--- nearit-ui/src/main/java/com/nearit/ui_bindings/utils/images/ImageDownloadListener.java
+++ nearit-ui/src/main/java/com/nearit/ui_bindings/utils/images/ImageDownloadListener.java
@@ -1,12 +1,11 @@
 package com.nearit.ui_bindings.utils.images;
 
-import android.graphics.Bitmap;
 import android.support.annotation.NonNull;
 
 /**
  * @author Federico Boschini
  */
 public interface ImageDownloadListener {
-    void onSuccess(@NonNull Bitmap bitmap);
+    void onSuccess(@NonNull Image image);
     void onError();
 }
