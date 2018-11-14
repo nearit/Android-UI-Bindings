6617121f65d80a41ce8a6691982455349f23d1e5
Federico Boschini <federico@nearit.com>
2018-11-14 12:37:31 +0100

    test(content): add content stub

---
+++ nearit-ui/src/test/java/com/nearit/ui_bindings/stubs/ContentStub.java
@@ -0,0 +1,8 @@
+package com.nearit.ui_bindings.stubs;
+
+import it.near.sdk.reactions.contentplugin.model.Content;
+
+/**
+ * @author Federico Boschini
+ */
+public class ContentStub extends Content {}
