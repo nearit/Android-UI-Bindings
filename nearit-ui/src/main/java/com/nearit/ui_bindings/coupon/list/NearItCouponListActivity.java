38f3fdeb780be447ed62d3f71a2884d1935cc2d7
Federico Boschini <federico@nearit.com>
2018-11-27 13:27:34 +0100

    feat(activities): make old activities extend base

--- nearit-ui/src/main/java/com/nearit/ui_bindings/coupon/list/NearItCouponListActivity.java
+++ nearit-ui/src/main/java/com/nearit/ui_bindings/coupon/list/NearItCouponListActivity.java
@@ -2,67 +2,19 @@ package com.nearit.ui_bindings.coupon.list;
 
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Rect;
-import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
-import android.view.MotionEvent;
 
 import com.nearit.ui_bindings.ExtraConstants;
-import com.nearit.ui_bindings.R;
 
 /**
  * @author Federico Boschini
  */
 
-public class NearItCouponListActivity extends AppCompatActivity {
-
-    private final static String TAG = "NearItCouponListActiv";
-    private boolean isEnableTapToClose;
-
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.nearit_ui_activity_coupon_list);
-
-        CouponListExtraParams extras = null;
-        Intent intent = getIntent();
-        if (intent != null &&
-                intent.hasExtra(ExtraConstants.EXTRA_FLOW_PARAMS)) {
-            extras = CouponListExtraParams.fromIntent(intent);
-            isEnableTapToClose = extras.isEnableTapOutsideToClose();
-            String activityTitle = extras.getActivityTitle();
-            if (activityTitle != null) {
-                setTitle(activityTitle);
-            }
-        }
-
-        getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.coupons_fragment_container, NearItCouponListFragment.newInstance(extras))
-                .commit();
-
-    }
+public class NearItCouponListActivity extends BaseCouponListActivity {
 
     @NonNull
     public static Intent createIntent(Context context, CouponListExtraParams params) {
         return new Intent(context, NearItCouponListActivity.class)
                 .putExtra(ExtraConstants.EXTRA_FLOW_PARAMS, params);
-    }
-
-    @Override
-    public boolean dispatchTouchEvent(MotionEvent ev) {
-        Rect dialogBounds = new Rect();
-        getWindow().getDecorView().getHitRect(dialogBounds);
-
-        if (!dialogBounds.contains((int) ev.getX(), (int) ev.getY())) {
-            if (!isEnableTapToClose) {
-                return true;
-            }
-            finish();
-        }
-        return super.dispatchTouchEvent(ev);
     }
 }
--- nearit-ui/src/main/java/com/nearit/ui_bindings/inbox/NearITNotificationHistoryActivity.java
+++ nearit-ui/src/main/java/com/nearit/ui_bindings/inbox/NearITNotificationHistoryActivity.java
@@ -2,43 +2,15 @@ package com.nearit.ui_bindings.inbox;
 
 import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 
-import com.nearit.ui_bindings.ExtraConstants;
-import com.nearit.ui_bindings.R;
+/**
+ * @author Federico Boschini
+ */
+public class NearITNotificationHistoryActivity extends BaseNotificationHistoryActivity {
 
-public class NearITNotificationHistoryActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.nearit_ui_activity_notification_history);
-
-        NotificationHistoryExtraParams extras = null;
-        Intent intent = getIntent();
-        if (intent != null &&
-                intent.hasExtra(ExtraConstants.EXTRA_FLOW_PARAMS)) {
-            extras = intent.getParcelableExtra(ExtraConstants.EXTRA_FLOW_PARAMS);
-            String activityTitle = extras.getActivityTitle();
-            if (activityTitle != null) {
-                setTitle(activityTitle);
-            }
-        }
-
-        getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.notifications_fragment_container,
-                        NearITNotificationHistoryFragment.newInstance(extras))
-                .commit();
+    public static Intent createIntent(Context context, NotificationHistoryExtraParams params) {
+        Intent intent = new Intent(context, NearITNotificationHistoryActivity.class);
+        return BaseNotificationHistoryActivity.addExtras(intent, params);
     }
 
-    @NonNull
-    public static Intent createIntent(Context context, NotificationHistoryExtraParams extras) {
-        return new Intent(context, NearITNotificationHistoryActivity.class)
-                .putExtra(ExtraConstants.EXTRA_FLOW_PARAMS, extras);
-    }
 }
